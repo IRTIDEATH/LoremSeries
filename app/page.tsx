af2e856a76325e45d119557e5f8ad7fa87ccbf3e
IRTIDEATH <enderjhass@gmail.com>
2024-10-30 16:24:39 +0700

    conflict #2

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { TextEffect } from "@/components/ui/Text-effect";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="w-full mx-auto px-8">
       <TextEffect per="word" as="h1" preset="blur" className="text-4xl">
         Lorem Your Ipsum
       </TextEffect>
