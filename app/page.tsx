65ee23c2a29ede5c44e2ab0073e3bbdfa89e173f
IRTIDEATH <enderjhass@gmail.com>
2024-11-10 19:03:01 +0700

    feat: create carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import CarouselContainer from "@/components/Carousel";
 import Hero from "@/components/pages/Hero";
 
 export default function Home() {
   return (
-    <div className="w-full mx-auto px-8">
+    <div className="w-full mx-auto px-20">
       <Hero/>
+      <CarouselContainer/>
     </div>
   );
 }
---
+++ components/pages/Gallery.tsx
@@ -0,0 +1,12 @@
+import CarouselContainer from "../Carousel"
+
+
+const Gallery = () => {
+  return (
+    <section>
+        <CarouselContainer/>
+    </section>
+  )
+}
+
+export default Gallery
